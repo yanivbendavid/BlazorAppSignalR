7ec4bf1d013f720d34a1682563e64d7fff8f4f18
Yaniv Ben David <yanivbd@gmail.com>
2022-06-13 13:51:14 +0300

    Init

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorAppSignalR.Hubs;
+
+public class ChatHub:Hub
+{
+    public Task SendMessage(string user, string message)
+    {
+        return Clients.All.SendAsync("ReceiveMethod", user, message);
+    }
+}
